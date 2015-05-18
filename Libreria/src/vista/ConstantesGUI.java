d92f22f214bfb93bff566966efe05a24f490c326
Diego Vargas <diego.vargas02@uptc.edu.co>
2015-05-18 18:33:28 -0500

    Cree el dialogo crear libro declarando sus componentes y cree algunas constantes en la clase constantes GUI

--- Libreria/src/vista/ConstantesGUI.java
+++ Libreria/src/vista/ConstantesGUI.java
@@ -1,5 +1,12 @@
 package vista;
 
 public class ConstantesGUI {
+	
+	//Constantes tamaños
+	public static final int ANCHO_DIALOGO = 300;
+	public static final int ALTO_DIALOGO = 700;
+	
+	//Cosntantes Titulos
+	public static final String T_CREAR_LIBRO = "Crear Libro";
 
 }
---
+++ Libreria/src/vista/DialogoCrearLibro.java
@@ -0,0 +1,37 @@
+package vista;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import controlador.Controlador;
+import modelo.entidades.Autor;
+import modelo.entidades.EnumGeneroLibro;
+
+public class DialogoCrearLibro extends JDialog{
+
+	private static final long serialVersionUID = 1L;
+	private JLabel lbId;
+	private JTextField txId;
+	private JLabel lbNombre;
+	private JTextField txNombre;
+	private JLabel lbEditorial;
+	private JTextField txEditorial;
+	private JLabel lbAutor;
+	private JComboBox<Autor> autor;
+	private JLabel lbValor;
+	private JTextField txValor;
+	private JLabel lbGenero;
+	private JComboBox<EnumGeneroLibro> genero;
+	private JLabel lbPortada;
+	private JButton btnSubirImagenPortada;
+	private JLabel lbRuta;
+	
+	public DialogoCrearLibro(Controlador controlador) {
+		setTitle(ConstantesGUI.T_CREAR_LIBRO);
+		setSize(ConstantesGUI.ANCHO_DIALOGO, ConstantesGUI.ALTO_DIALOGO);
+		setLayout(null);
+	}
+}
