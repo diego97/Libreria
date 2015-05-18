6165ae9b89121c526bb1d049ae3e3951fbe4efc5
Diego Vargas <diego.vargas02@uptc.edu.co>
2015-05-18 18:57:11 -0500

    Alg

--- Libreria/src/vista/ConstantesGUI.java
+++ Libreria/src/vista/ConstantesGUI.java
@@ -1,21 +1,91 @@
 package vista;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class ConstantesGUI {
+
+	/**
+	 * Constantes Ventana Principal
+	 */
+	public static final String T_TITULO_VENTANA = "Gestor Sitios";
+	public static final String I_ICONO_VENTANA = "/recursos.images";
+	public static final String BTN_MOSTRAR = "Mostrar Tabla";
+	public static final int VENTANA_ANCHO = 700;
+	public static final int VENTANA_ALTO = 400;
+	/**
+	 * Constantes Menu
+	 */
 	
 	//Constantes tamaños
 	public static final int ANCHO_DIALOGO = 500;
 	public static final int ALTO_DIALOGO = 700;
+	public static final String T_MENU_ARCHIVO = "Archivo";
+	public static final String T_ITEM_NUEVO = "Nuevo";
+	public static final String T_EDITAR_ITEM = "Editar";
+	public static final String T_BORRAR_ITEM = "Borrar";
+	public static final String T_BUSCAR_ITEM = "Buscar";
+	public static final String T_ABRIR_ARCHIVO = "Abrir";
+	public static final String T_GUARDAR_ARCHIVO = "Guardar";
+	/**
+	 * Constantes Dialogos
+	 */
+	public static final String T_DIALOGO_CREAR_SITIO = "Crear Sitio";
+	public static final String T_NOMBRE = "Nombre";
+	public static final String T_VALOR = "Valor";;
+	public static final String T_INFORMACION = "Informacion";
+	public static final String T_IMAGEN = "Imagen";
+	public static final String BTN_CREAR = "Crear";
+	public static final String BTN_EDITAR = "Editar";
 	
 	//Cosntantes Titulos
 	public static final String T_CREAR_LIBRO = "Crear Libro";
 	
 	//Constantes labels
 	public static final String T_ID = "Id:";
-	public static final String T_NOMBRE = "Nombre:";
 	public static final String T_EDITORIAL = "Editorial:";
 	public static final String T_AUTOR = "Autor:";
-	public static final String T_VALOR = "Valor:";
 	public static final String T_GENERO = "Genero:";
 	public static final String T_PORTADA = "Portada:";
 
+
+	/**
+	 * Fuentes
+	 */
+	public static final Font FONT = new Font ("Ubuntu Light",Font.PLAIN,14 ) ; 
+	public static final Font TITULO_FONT = new Font ("Ubuntu Light",Font.BOLD,14 ) ; 
+	public static final Font FONTBOLD = new Font ("Ubuntu Light",Font.BOLD,14 ) ; 
+	/**
+	 * Constantes Imagenes
+	 */
+	public static final ImageIcon NUEVO_IMAGE  = new ImageIcon("src/pics/new.png");
+	public static final ImageIcon EDITAR_IMAGE  = new ImageIcon("src/pics/edit.png");
+	public static final ImageIcon BORRAR_IMAGE  = new ImageIcon("src/pics/remove.png");
+	public static final ImageIcon BUSCAR_IMAGE  = new ImageIcon("src/pics/search.png");
+	public static final ImageIcon ABRIR_IMAGE  = new ImageIcon("src/pics/open.png");
+	public static final ImageIcon GUARDAR_IMAGE  = new ImageIcon("src/pics/save.png");
+	public static final ImageIcon M_NUEVO_IMAGE  = new ImageIcon("src/pics/main/crear.png");
+	public static final ImageIcon M_EDITAR_IMAGE  = new ImageIcon("src/pics/main/editar.png");
+	public static final ImageIcon M_BORRAR_IMAGE  = new ImageIcon("src/pics/main/borrar1.png");
+	public static final ImageIcon M_BUSCAR_IMAGE  = new ImageIcon("src/pics/main/buscar.png");
+	public static final ImageIcon M_GUARDAR_IMAGE  = new ImageIcon("src/pics/main/borrar.png");
+	public static final ImageIcon M_ABRIR_IMAGE  = new ImageIcon("src/pics/main/abrir.png");
+	public static final ImageIcon TABLE_IMAGE  = new ImageIcon("src/pics/table.png");
+	public static final ImageIcon CHECK_IMAGE  = new ImageIcon("src/pics/check.png");
+	public static final ImageIcon INFORMATION_IMAGE  = new ImageIcon("src/pics/information.png");
+	public static final ImageIcon NAME_IMAGE  = new ImageIcon("src/pics/name.png");
+	public static final ImageIcon PRICE_IMAGE  = new ImageIcon("src/pics/price.png");
+	public static final ImageIcon SITE_IMAGE  = new ImageIcon("src/pics/sites.png");
+	public static final ImageIcon FONDO_IMAGE  = new ImageIcon("src/pics/fondo.png");
+	
+	/**
+	 * TamaÃ±os Botones
+	 */
+	public static final int ALTO_BOTON = 10;
+	public static final int ANCHO_BOTON = 10;
+	/**
+	 * Constantes Colores
+	 */
+	public static final String MORADO_COLOR  = "#292939";
+	public static final String AZUL_COLOR  = "#7F8BA5";
+	
 }
