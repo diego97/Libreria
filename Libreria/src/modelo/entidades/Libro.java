ccee8af97c07fc81dd72d01d92cac7064838611d
Robinson C: <stefa@CanFly>
2015-05-18 17:38:08 +0000

    sE AGREGO cLASE lIBRO Y EL ENUM

--- Libreria/src/modelo/entidades/Libro.java
+++ Libreria/src/modelo/entidades/Libro.java
@@ -1,8 +1,72 @@
 package modelo.entidades;
 
-public class Libro {
-	
-	public double valor;
-	
+import java.util.Date;
 
-}
+public class Libro {
+	private int id;
+	private String nombre;
+	private String editorial;
+	private Autor autor;
+	private Date fechaPublicacion;
+	private Double valor;
+	private EnumGeneroLibro genero;
+	private String imagenPortada;
+	private int copiasVendidas;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEditorial() {
+		return editorial;
+	}
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	public Date getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+	public void setFechaPublicacion(Date fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public EnumGeneroLibro getGenero() {
+		return genero;
+	}
+	public void setGenero(EnumGeneroLibro genero) {
+		this.genero = genero;
+	}
+	public String getImagenPortada() {
+		return imagenPortada;
+	}
+	public void setImagenPortada(String imagenPortada) {
+		this.imagenPortada = imagenPortada;
+	}
+	public int getCopiasVendidas() {
+		return copiasVendidas;
+	}
+	public void setCopiasVendidas(int copiasVendidas) {
+		this.copiasVendidas = copiasVendidas;
+	}
+	
+	}
+
